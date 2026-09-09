25ef0b8c3be0d26874266229f520e0e4e41441e3
Karina ☆ <karina.dev@proton.me>
2026-09-09 17:37:05 -0300

    feat calculadora adicionada

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,26 @@
-#Input
+valor = float(input("Digite o valor: "))
 
-#Processing
 
-#Output
+
+
+if valor < 200:
+   valor1 = 5 / 100 * valor
+   result1 = valor - valor1
+   print("Você ganhou 5% de desconto!")
+   print(f"Portanto, o valor de sua compra foi de {result1:2f}")
+
+elif valor >= 200 and valor <= 300:
+     valor2 = 10 / 100 * valor
+     result2 = valor - valor2
+     print("Você ganhou 10% de desconto!")
+     print(f"Portanto, o valor de sua compra foi de {result2:.2f}")
+
+elif valor >= 300:
+     valor3 = 15 / 100 * valor
+     result3 = valor - valor3
+     print("Você ganhou 15% de desconto!")
+     print(f"Portanto, o valor de sua compra foi de {result3:.2f}")
+
+else:
+    print("sem desconto, si lascou")
 
